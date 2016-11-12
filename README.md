005ad76e03f2a4a798bb98258ed6c9d95e6db04c
GengAndre <gengze@meituan.com>
2016-11-12 11:52:52 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## gz-tpl-loader
+
+webpack loader for gz-util-ui-toast
+
+usage:
+  require('**.tpl')(data);
--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function (source) {
-  return ('var tpl = require(\'util-tool-tpl\').tpl;\n  module.exports = function (data) {\n    return tpl(\'' + source + '\')(data);\n  };').replace(/\r|\n/g, '');
+  return ('var tpl = require(\'gz-util-tool-tpl\').tpl;\n  module.exports = function (data) {\n    return tpl(\'' + source + '\')(data);\n  };').replace(/\r|\n/g, '');
 };
